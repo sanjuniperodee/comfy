d6a47107c0bb863cb5abc49ba30b13ed8a1e0497
sanjuniperodee <87779571856b@gmail.com>
2023-06-21 23:29:22 +0600

    first commit

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.staticfiles.views import serve
+from django.urls import path, re_path
+from .views import *
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', home, name='home'),
+]
---
+++ core/views.py
@@ -0,0 +1,8 @@
+import requests
+from django.conf import settings
+from django.shortcuts import render, get_object_or_404
+
+# stripe.api_key = settings.STRIPE_SECRET_KEY
+
+def home(request):
+    return render(request, 'index.html')
